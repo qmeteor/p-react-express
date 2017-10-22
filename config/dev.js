9d7ff2f2bb735ca0f05ff2ce25f8a414a6313c53
Bien Pham <pham_bv@hotmail.com>
2017-10-22 13:23:04 -0400

    added config files

---
+++ config/dev.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Bien on 2017-09-30.
+ */
+module.exports = {
+    googleClientID: '539548981476-uq8b2s0o3d2ge158llgsflg5833jhgnu.apps.googleusercontent.com',
+    googleClientSecret: 'E1UJm7zL4kzs5eOvTN2I0Ihi',
+    facebookClientID: '',
+    facebookClientSecret: '',
+    mongoURI: 'mongodb://bien:password@ds127105.mlab.com:27105/authentication-dev',
+    cookieKey: 'laksjdfojasdlkfjo;sojd;j;alsdjf;ojasdofjieioals'
+};
